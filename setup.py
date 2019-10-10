6a0d1119fbc2625a4aa20b092596be0b1d03553c
Benjamin Delmée <Benjamin.Delmee@fr.ey.com>
2019-10-10 14:53:20 +0200

    doc: add package documentation

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ from setuptools import setup
 
 setup(name='safecsv',
     version='1.0.0',
-    description='Make data integration more reliable')
+    description='Make data integration more reliable',
+    author='Benjamin Delmee',
+    author_email='benjamindelmee@gmail.com',
+    license='MIT',
+    zip_safe=False
+)
